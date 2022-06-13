adcefbe38174140bf9c30d2220ff2fba76e178f8
sim <simmorsal@gmail.com>
2022-06-14 00:10:20 +0430

    .

--- components/PortfolioContent/index.tsx
+++ components/PortfolioContent/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { css } from "@emotion/react";
 import { headerHeight } from "../Header";
+import { Grid } from "@mui/material";
 
 interface Props {}
 
@@ -10,16 +11,20 @@ export default function PortfolioContent(props: Props) {
       css={css`
         width: 100%;
         height: 100%;
+        min-height: calc(100vh - 2px);
         //border: 1px solid #484848;
         font-weight: bold;
         text-align: center;
         display: flex;
         flex-direction: column;
         justify-content: center;
-        padding: ${headerHeight}px 24px 24px;
+        align-items: center;
+        padding: ${headerHeight}px 24px 0px;
       `}
     >
-      Portfolio
+      <Grid container>
+        <Grid item xs={12} sm={6}></Grid>
+      </Grid>
     </div>
   );
 }
