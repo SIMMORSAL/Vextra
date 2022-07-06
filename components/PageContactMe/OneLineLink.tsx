97d75758c52e5cc1c504bd6f32fc1d60459e5bc5
sim <simmorsal@gmail.com>
2022-07-06 10:02:26 +0430

    .

--- components/PageContactMe/OneLineLink.tsx
+++ components/PageContactMe/OneLineLink.tsx
@@ -4,8 +4,6 @@ import EmailIcon from "@mui/icons-material/Email";
 import { ContactGroup } from "../../data/models/contactGroup";
 
 interface Props {
-  // text: string;
-  // href: string;
   contact: ContactGroup;
 }
 
@@ -40,66 +38,89 @@ export default function OneLineLink(props: Props) {
         css={css`
           width: 30px;
           height: 30px;
-          margin-right: 12px;
+          margin-right: 8px;
           display: flex;
           flex-direction: row-reverse;
           align-items: center;
         `}
       >
-        <a
-          href={props.contact.links[0].href}
-          target={_target}
+        <div
           css={css`
             display: flex;
-            justify-content: center;
-            align-items: center;
-            width: fit-content;
-
-            transition: 140ms ease;
-            transition-property: width, height, margin-right, opacity, transform;
+            flex-direction: row-reverse;
           `}
-          rel="noreferrer"
         >
-          <i
-            className={props.contact.links[0].icon}
-            css={css`
-              width: 24px;
-              cursor: pointer;
-              opacity: ${beginAnimation ? 1 : 0};
-              margin-right: ${beginAnimation ? 0 : 16}px;
-              text-align: right;
-              font-size: 24px;
-              transform: scale(1.1);
+          <div>
+            <a
+              target={_target}
+              href={props.contact.links[0].href}
+              rel={"noreferrer"}
+            >
+              <i
+                className={props.contact.links[0].icon}
+                css={css`
+                  width: 30px;
+                  //height: 30px;
+                  cursor: pointer;
+                  opacity: ${beginAnimation ? 1 : 0};
+                  margin: 16px 0;
+                  padding: 0;
+                  //background-color: red;
+                  //background-color: red;
+                  transform: translateX(${beginAnimation ? 0 : "-50%"});
+                  text-align: center;
+                  font-size: 150%;
 
-              transition: 140ms ease;
-              transition-property: width, height, margin-right, opacity, transform,
-                font-size;
+                  transition: 140ms ease;
+                  transition-property: width, height, margin-top, opacity, transform,
+                    font-size, color;
 
-              :hover {
-                width: 30px;
-                font-size: 30px;
-                //transform: scale(1.47);
-              }
-            `}
-          />
-          {/*<EmailIcon*/}
-          {/*  css={css`*/}
-          {/*    width: 24px;*/}
-          {/*    height: 24px;*/}
-          {/*    cursor: pointer;*/}
-          {/*    margin-right: ${beginAnimation ? 0 : 16}px;*/}
-          {/*    opacity: ${beginAnimation ? 1 : 0};*/}
+                  :hover {
+                    transform: scale(1.3);
+                    width: 42px;
+                  }
+                `}
+              />
+            </a>
+          </div>
+        </div>
+        {/*<a*/}
+        {/*  href={props.contact.links[0].href}*/}
+        {/*  target={_target}*/}
+        {/*  css={css`*/}
+        {/*    display: flex;*/}
+        {/*    justify-content: center;*/}
+        {/*    align-items: center;*/}
+        {/*    width: fit-content;*/}
 
-          {/*    transition: 140ms ease;*/}
-          {/*    transition-property: width, height, margin-right, opacity;*/}
+        {/*    transition: 140ms ease;*/}
+        {/*    transition-property: width, height, margin-right, opacity, transform;*/}
+        {/*  `}*/}
+        {/*  rel="noreferrer"*/}
+        {/*>*/}
+        {/*  <i*/}
+        {/*    className={props.contact.links[0].icon}*/}
+        {/*    css={css`*/}
+        {/*      width: 24px;*/}
+        {/*      cursor: pointer;*/}
+        {/*      opacity: ${beginAnimation ? 1 : 0};*/}
+        {/*      margin-right: ${beginAnimation ? 0 : 16}px;*/}
+        {/*      text-align: right;*/}
+        {/*      font-size: 24px;*/}
+        {/*      transform: scale(1.1);*/}
 
-          {/*    :hover {*/}
-          {/*      width: 30px;*/}
-          {/*      height: 30px;*/}
-          {/*    }*/}
-          {/*  `}*/}
-          {/*/>*/}
-        </a>
+        {/*      transition: 140ms ease;*/}
+        {/*      transition-property: width, height, margin-right, opacity, transform,*/}
+        {/*        font-size;*/}
+
+        {/*      :hover {*/}
+        {/*        width: 30px;*/}
+        {/*        font-size: 30px;*/}
+        {/*        //transform: scale(1.47);*/}
+        {/*      }*/}
+        {/*    `}*/}
+        {/*  />*/}
+        {/*</a>*/}
       </div>
       <div
         css={css`
