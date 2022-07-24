103a3116e506c616411dece4264fba533cf72b16
simmorsal <simmorsal@gmail.com>
2022-07-24 11:06:09 +0430

    .

--- data/local/portfolio/getDataProject2.ts
+++ data/local/portfolio/getDataProject2.ts
@@ -73,6 +73,9 @@ and we named it
             text: `
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida facilisis urna et scelerisque. Nulla dignissim ac eros ut lobortis. Donec eleifend quis nisi in fringilla. Integer rhoncus, leo vulputate dictum elementum, odio velit condimentum massa, a eleifend orci tellus sed ligula. Duis convallis volutpat augue, id volutpat augue porttitor eu. Donec risus mi, semper non dictum finibus, efficitur nec nunc. Sed iaculis odio nec efficitur posuere. Aliquam porta lectus orci, vel cursus sem laoreet sit amet. Ut viverra nulla at scelerisque aliquam. Duis venenatis arcu nunc, sit amet luctus augue egestas sit amet. Sed eget est est. Proin quis lectus eget magna volutpat consequat at luctus ex. Quisque posuere iaculis vestibulum. Phasellus non interdum nibh. Ut nisi orci, cursus nec euismod at, pharetra quis massa."
 
+\- Cisero
+
+\` \`  
 This product comes in a variety of colors
 `,
           },
