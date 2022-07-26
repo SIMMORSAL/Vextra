6dd10e29c84b4bb3b790bfd88b11a1a95f1a4cd9
sim <simmorsal@gmail.com>
2022-07-26 17:39:16 +0430

    .

--- components/RichContent/Markdown/MarkdownSyntaxTheme.ts
+++ components/RichContent/Markdown/MarkdownSyntaxTheme.ts
@@ -40,7 +40,7 @@ export const themeLight = {
     background: "#50321c",
     // background: "rgb(188,130,82)",
     borderRadius: "8px",
-    border: "3px solid #2F2218",
+    border: "1px solid #2F2218",
   },
   'pre[class*="language-"]::-moz-selection': {
     color: "#282828",
