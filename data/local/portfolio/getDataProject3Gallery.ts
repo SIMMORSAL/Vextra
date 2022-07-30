3333a0dfb3fe1efd28e81bd98edf1d6f41c26ca8
simmorsal <simmorsal@gmail.com>
2022-07-30 16:03:32 +0430

    .

--- data/local/portfolio/getDataProject3Gallery.ts
+++ data/local/portfolio/getDataProject3Gallery.ts
@@ -1,6 +1,49 @@
 import { RichChunkModel } from "../../models/rich-chunk/RichChunkModel";
 
 export function getDataProject3Gallery(): RichChunkModel[] {
+  
+  const refs = [
+    "Katerina Kerdi",//
+    "Giuseppe Mondi",//
+    "Artiom Vallat",//
+    "Allyson Beaucourt",
+    "Roma Ryabchenko",//
+    "Andre Furtado",
+    "Simon Wilkes",
+    "Benjamin Davies",//
+    "Paul Summers",
+    "Hojjat Aghaei",
+    "Andre Furtado",
+    "Ales Krivec",
+    "Qingbao Meng",
+    "Thomas Morse",
+    "Adam Kool",
+    "Asal Lotfi",
+    "Johannes Plenio",
+    "Andre Furtado",
+  ];
+  
+  const refLinks = [
+    "https://unsplash.com/@katekerdi",
+    "https://unsplash.com/@masinutoscana",
+    "https://unsplash.com/@virussinside",
+    "https://unsplash.com/@daymnous",
+    "https://unsplash.com/@n3moy",
+    "https://unsplash.com/@andre_furtado",
+    "https://unsplash.com/@simonfromengland",
+    "https://unsplash.com/@bendavisual",
+    "https://unsplash.com/@paul_s",
+    "https://unsplash.com/@hojjat1995",
+    "https://unsplash.com/@andre_furtado",
+    "https://unsplash.com/@aleskrivec",
+    "https://unsplash.com/@ideasboom",
+    "https://unsplash.com/@timorse",
+    "https://unsplash.com/@adamkool",
+    "https://unsplash.com/@asal_lotfi",
+    "https://unsplash.com/@jplenio",
+    "https://unsplash.com/@andre_furtado",
+  ];
+  
   const images = Array(18).fill(0).map((_, i) => {
       return {
         freeSpaceTop: "24px",
@@ -21,7 +64,7 @@ export function getDataProject3Gallery(): RichChunkModel[] {
                 duration: 400,
               },
               text: `
-${i}
+##### [${refs[i]}](${refLinks[i]})
 `,
             },
           ],
