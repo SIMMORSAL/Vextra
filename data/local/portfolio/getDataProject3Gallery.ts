6402516a4ba1c1a3f2c341bd0b6b1df63d29439c
sim <simmorsal@gmail.com>
2022-07-30 16:18:55 +0430

    .

--- data/local/portfolio/getDataProject3Gallery.ts
+++ data/local/portfolio/getDataProject3Gallery.ts
@@ -1,16 +1,15 @@
 import { RichChunkModel } from "../../models/rich-chunk/RichChunkModel";
 
 export function getDataProject3Gallery(): RichChunkModel[] {
-  
   const refs = [
-    "Katerina Kerdi",//
-    "Giuseppe Mondi",//
-    "Artiom Vallat",//
+    "Katerina Kerdi", //
+    "Giuseppe Mondi", //
+    "Artiom Vallat", //
     "Allyson Beaucourt",
-    "Roma Ryabchenko",//
+    "Roma Ryabchenko", //
     "Andre Furtado",
     "Simon Wilkes",
-    "Benjamin Davies",//
+    "Benjamin Davies", //
     "Paul Summers",
     "Hojjat Aghaei",
     "Andre Furtado",
@@ -22,7 +21,7 @@ export function getDataProject3Gallery(): RichChunkModel[] {
     "Johannes Plenio",
     "Andre Furtado",
   ];
-  
+
   const refLinks = [
     "https://unsplash.com/@katekerdi",
     "https://unsplash.com/@masinutoscana",
@@ -43,58 +42,59 @@ export function getDataProject3Gallery(): RichChunkModel[] {
     "https://unsplash.com/@jplenio",
     "https://unsplash.com/@andre_furtado",
   ];
-  
-  const images = Array(18).fill(0).map((_, i) => {
+
+  const images = Array(18)
+    .fill(0)
+    .map((_, i) => {
       return {
         freeSpaceTop: "24px",
         bigImage: {
           path: `/images/portfolio/gallery/g${i}.jpg`,
+          aspectRatio: i === 0 ? "2400/1600" : i === 1 ? "2400/3600" : undefined,
           animation: {
             animateOnScroll: i !== 0,
             animateFrom: "bottom",
           },
         },
         textsBottom: [
-            {
-              align: "center",
-              aspectRatio: i === 0 ? "2400/1600" : i === 1 ? "2400/3600" : undefined,
-              animation: {
-                animateOnScroll: true,
-                animateFrom: "top",
-                translateDistance: 5,
-                duration: 400,
-              },
-              text: `
+          {
+            align: "center",
+            animation: {
+              animateOnScroll: true,
+              animateFrom: "top",
+              translateDistance: 5,
+              duration: 400,
+            },
+            text: `
 ##### [${refs[i]}](${refLinks[i]})
 `,
-            },
-          ],
+          },
+        ],
       } as RichChunkModel;
-    }
-  );
-  
+    });
+
   return (
     [
       {
         minHeight: "20vh",
         textsTop: [
-            {
-              align: "center",
-              site: "2em",
-              text: "# Gallery",
-            }
-          ],
+          {
+            align: "center",
+            site: "2em",
+            text: "# Gallery",
+          },
+        ],
       },
       {
         backgroundColor: "#eee",
         textsTop: [
-            {
-              align: "center",
-              text: `
+          {
+            align: "center",
+            text: `
 One of my hobbies is climbing mountains and taking pictures. Please enjoy some of them
 `,
-            }
-          ],
+          },
+        ],
       },
       ...images,
       {
