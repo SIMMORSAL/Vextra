cce17e71090d3fea6672cd2341310f52eed58b14
simmorsal <simmorsal@gmail.com>
2022-07-31 12:45:20 +0430

    .

--- data/local/portfolio/getDataRichContentHandbook.ts
+++ data/local/portfolio/getDataRichContentHandbook.ts
@@ -84,7 +84,7 @@ export function getDataRichContentHandbook(): RichChunkModel[] {
             text: ` 
 
 This is a full blown test in demonstration of Rich Chunks abilities. 
-You can find the data file of this page 
+You can find the data file of this and all the other pages
 in ${getInlineCode(
               "/data/local/portfolio/&#8291;getDataRichContentHandbook.ts",
               "green"
