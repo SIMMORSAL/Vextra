3dcacb6b4c6146dbf8d002a1bb80e1cefb381f9c
sim <simmorsal@gmail.com>
2022-08-13 11:56:56 +0430

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,18 +17,21 @@ export default function _Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>
-          Vextra - Elegant and Animated Portfolio Website
-        </title>
-        <meta name="description" content="A highly customizable platform ready to be a portfolio page, and become a lot more with some of your own components."/>,
-        </title>
-        <meta name="og:title" content="Vextra - Elegant and Animated Portfolio Website"/>,
-        </title>
-        <meta name="og:url" content="/"/>,
-        </title>
-        <meta name="og:description" content="A highly customizable platform ready to be a portfolio page, and become a lot more with some of your own components."/>,
-        </title>
-        <meta name="og:image" content="/images/preview.jpg"/>,
+        <title>Vextra - Elegant and Animated Portfolio Website</title>
+        <meta
+          name="description"
+          content="A highly customizable platform ready to be a portfolio page, and become a lot more with some of your own components."
+        />
+        <meta
+          name="og:title"
+          content="Vextra - Elegant and Animated Portfolio Website"
+        />
+        <meta name="og:url" content="/" />
+        <meta
+          name="og:description"
+          content="A highly customizable platform ready to be a portfolio page, and become a lot more with some of your own components."
+        />
+        <meta name="og:image" content="/images/preview.jpg" />
       </Head>
       <body>
         <Main />
